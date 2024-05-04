01286a5c3b5bf679e4fce2589b77403b01ac322c
ArnauMartinezAra <arnau.mara@gmail.com>
2024-05-04 15:37:57 +0200

    Hola

--- f_api_events.py
+++ f_api_events.py
@@ -2,7 +2,6 @@
 
 import holidays
 import sys
-import streamlit as st
 date = "06/05/2024"
 timezone = "Madrid/Europe"
 api_key = 'YFAuf4V91WCV37YHqNXSZzsATzlEfOOI'
@@ -23,13 +22,7 @@ if __name__ == "__main__":
     except Exception as e:
         print(f"An error has ocurred {e}")
 
-@st.cache_data
-def api_call(city, date):
-    tz = city + "/Europe"
-    dte = str(date)
-    client = holidays.client(api_key)
-    events = client.getEvents(timezone=tz, date=dte)
-    return events
+
 
 
 
